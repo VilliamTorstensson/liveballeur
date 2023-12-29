0ad79be039cd4fdf0a612d9ef67ee382aea69e0e
VilliamTorstensson <135708823+VilliamTorstensson@users.noreply.github.com>
2023-12-29 15:41:13 +0100

    test

--- app/components/TextSlider.jsx
+++ app/components/TextSlider.jsx
@@ -5,29 +5,29 @@ export default function TextSlider () {
       <div className="flex gap-2 logos-slide2">
         <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Foto <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
-        <h1 className="text-[#636363] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Social media management  <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Film <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
+        <h1 className="text-[#000] text-4xl md:text-5xl lg:6xl xl:text-8xl font-normal w-content">Consulting <RxBorderSolid className='text-4xl xl:text-5xl lg:text-6xl md:text-8xl inline-block' /></h1>
         
        
        
