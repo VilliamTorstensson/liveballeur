8cadced3a1db5a856de8999590c6b131ebac5451
VilliamTorstensson <135708823+VilliamTorstensson@users.noreply.github.com>
2023-12-30 19:14:40 +0100

    test

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,14 +1,13 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { RxBorderSolid } from 'react-icons/rx'
-import { motion } from 'framer-motion'
+
 export default function Hero () {
 
   const hero = [
     {
       id: 1,
-      title: 'Vi skapar meningsfullt innehåll med ett syfte, skapa meningsfulla relationer',
+      title: 'Vi skapar meningsfullt innehåll med ett syfte',
     
     }
   ]
@@ -29,7 +28,7 @@ export default function Hero () {
         return (
           <div
            key={item.id} className='flex flex-col lg:gap-10 2xl:gap-20 gap-8 '>
-          <h1 key={item.id} className=' w-full   py-1  text-[1.5rem] font-semibold  animation sm:text-4xl md:text-4xl lg:text-5xl xl:text-7xl '>{item.title}</h1>
+          <h1 key={item.id} className=' w-full   py-1  text-[1.8rem] font-normal  animation sm:text-4xl md:text-4xl lg:text-5xl xl:text-9xl '>{item.title}</h1>
           <Image src="/herocomp.jpg" alt="Picture of the author" width={2000} height={1000} className='  md:block hidden animation2' />
           <Image src="/heromob.jpg" alt="Picture of the author" width={2000} height={1000} className='  md:hidden block animation2' />
           <Link href="mailto:Samuel@balleurproduction.com" className='button w-min animation2 animation2' >Boka ett möte!</Link>
--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,24 @@
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { Wix_Madefor_Display} from 'next/font/google'
+import LocalFont from 'next/font/local'
 
 
-
+const Neue = LocalFont({
+  name: 'Neue',
+  src: '/font/NeueRoman.ttf',
+})
 
 export const metadata = {
   title: 'Balleur production',
   description: 'Vi skapar innehåll för sociala medier, hemsidor och andra digitala plattformar. Vi hjälper dig att skapa en stark digital närvaro.',
 }
-const MyFont = Wix_Madefor_Display({
-  subsets: ['latin'],
-  display: 'swap',
 
-})
 
 export default function RootLayout({ children }) {
   return (
     <html className='scroll-smooth' lang="en">
-      <body className={MyFont.className}>
+      <body className={Neue.className}>
         <Navbar />
         {children}
         <Footer />
