7b1044d1690ca7382d376b1826e6fbc81a1e9a06
VilliamTorstensson <135708823+VilliamTorstensson@users.noreply.github.com>
2023-12-30 12:20:29 +0100

    e

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { GeistSans } from 'geist/font/sans';
+
 import { Wix_Madefor_Display} from 'next/font/google'
 
 
